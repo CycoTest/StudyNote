66c398dcfd745ab7fbfad20565046565ff17053a
CycoTest <aceldamadk@gmail.com>
2024-02-29 21:47:08 +0900

    Skill Tech first one

---
+++ ST_1/src/test/java/personal/practice/skill_tech/SkillTechApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.practice.skill_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
