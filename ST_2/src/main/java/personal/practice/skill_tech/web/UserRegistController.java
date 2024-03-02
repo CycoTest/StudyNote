0304664ba8a5e760ee343a168f42bf66364355b5
CycoTest <aceldamadk@gmail.com>
2024-03-03 00:39:22 +0900

    Registration Form by Thymeleaf

--- ST_2/src/main/java/personal/practice/skill_tech/web/UserRegistController.java
+++ ST_2/src/main/java/personal/practice/skill_tech/web/UserRegistController.java
@@ -2,6 +2,7 @@ package personal.practice.skill_tech.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,15 +17,21 @@ public class UserRegistController {
 
     private final UserService userService;
 
+    @ModelAttribute("user")
+    public UserRegirstDto userRegirstDto() {
+
+        return new UserRegirstDto();
+    }
+
     @GetMapping
-    public String showRegistrationForm() {
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new UserRegirstDto());
 
         return "/views/registration";
     }
 
     @PostMapping
     public String registerUserAccount(@ModelAttribute("user")UserRegirstDto regirstDto) {
-
         userService.save(regirstDto);
 
         return "redirect:/registration?success";
--- ST_2/src/main/java/personal/practice/skill_tech/web/dto/UserRegirstDto.java
+++ ST_2/src/main/java/personal/practice/skill_tech/web/dto/UserRegirstDto.java
@@ -2,10 +2,12 @@ package personal.practice.skill_tech.web.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserRegirstDto {
     private String firstName;
