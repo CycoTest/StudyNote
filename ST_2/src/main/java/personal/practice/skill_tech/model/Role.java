5507baf90480b85048eaade5197cc677868f182b
CycoTest <aceldamadk@gmail.com>
2024-03-02 16:41:01 +0900

    Make Model java class
    1. User & Role
    2. User
    - Collection<Role> roles
    - @ManyToMany, @JoinTable
    - Constructor without id

---
+++ ST_2/src/main/java/personal/practice/skill_tech/model/Role.java
@@ -0,0 +1,18 @@
+package personal.practice.skill_tech.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
