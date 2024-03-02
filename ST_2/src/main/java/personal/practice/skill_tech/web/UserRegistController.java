26666b2356c381c8cca08b32d6805fa80720c39d
CycoTest <aceldamadk@gmail.com>
2024-03-02 17:23:38 +0900

    Make Registration Form
    initialize registration.html

--- ST_2/src/main/java/personal/practice/skill_tech/web/UserRegistController.java
+++ ST_2/src/main/java/personal/practice/skill_tech/web/UserRegistController.java
@@ -2,7 +2,9 @@ package personal.practice.skill_tech.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import personal.practice.skill_tech.service.UserService;
 import personal.practice.skill_tech.web.dto.UserRegirstDto;
@@ -14,6 +16,13 @@ public class UserRegistController {
 
     private final UserService userService;
 
+    @GetMapping
+    public String showRegistrationForm() {
+
+        return "/views/registration";
+    }
+
+    @PostMapping
     public String registerUserAccount(@ModelAttribute("user")UserRegirstDto regirstDto) {
 
         userService.save(regirstDto);
