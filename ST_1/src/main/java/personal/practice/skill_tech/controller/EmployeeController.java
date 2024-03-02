9460417c65c0182694d354f4c71f99c752c44468
CycoTest <aceldamadk@gmail.com>
2024-03-02 14:25:09 +0900

    Employee List Board Column Ascending
    Add Ascending / Desending by each Column

--- ST_1/src/main/java/personal/practice/skill_tech/controller/EmployeeController.java
+++ ST_1/src/main/java/personal/practice/skill_tech/controller/EmployeeController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import personal.practice.skill_tech.model.Employee;
 import personal.practice.skill_tech.service.EmployeeService;
 
@@ -23,7 +20,7 @@ public class EmployeeController {
     @GetMapping("/")
     public String viewHome(Model model) {
         // default pageNo = 1
-        return findPaginated(1, model);
+        return findPaginated(1, "firstName", "asc", model);
 
 //        return "/views/index";
     }
@@ -68,16 +65,25 @@ public class EmployeeController {
         return "redirect:/";
     }
 
+    // /page/1?sortField=name&sortDir=asc
     @GetMapping("/page/{pageNo}") // pagination
-    public String findPaginated(@PathVariable(value = "pageNo") int pageNo, Model model) {
+    public String findPaginated(@PathVariable(value = "pageNo") int pageNo,
+                                @RequestParam("sortField") String sortField,
+                                @RequestParam("sortDir") String sortDir,
+                                Model model) {
         int pageSize = 5;
 
-        Page<Employee> page = employeeService.findPaginated(pageNo, pageSize);
+        Page<Employee> page = employeeService .findPaginated(pageNo, pageSize, sortField, sortDir);
         List<Employee> listEmployees = page.getContent();
 
         model.addAttribute("currentPage", pageNo);
         model.addAttribute("totalPages", page.getTotalPages());
         model.addAttribute("totalItems", page.getTotalElements());
+
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+
         model.addAttribute("listEmployees", listEmployees);
 
         return "/views/index";
--- ST_1/src/main/java/personal/practice/skill_tech/service/EmployeeService.java
+++ ST_1/src/main/java/personal/practice/skill_tech/service/EmployeeService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import personal.practice.skill_tech.model.Employee;
 import personal.practice.skill_tech.repository.EmployeeRepository;
@@ -50,9 +51,10 @@ public class EmployeeService {
     }
 
     // pagination for Employee List Board
-    public Page<Employee> findPaginated(int pageNo, int pageSize) {
-
-        Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
+    public Page<Employee> findPaginated(int pageNo, int pageSize,
+                                        String sortField, String sortDirection) {
+        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() : Sort.by(sortField).descending();
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
 
         return this.employeeRepository.findAll(pageable);
     }
