18ac7e8dc8de20de3f5d7568dbd1d56cd04f3734
CycoTest <aceldamadk@gmail.com>
2024-03-03 20:29:24 +0900

    Backend implementation for Login Feature

--- ST_2/src/main/java/personal/practice/skill_tech/service/UserService.java
+++ ST_2/src/main/java/personal/practice/skill_tech/service/UserService.java
@@ -1,9 +1,12 @@
 package personal.practice.skill_tech.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import personal.practice.skill_tech.model.Role;
 import personal.practice.skill_tech.model.User;
@@ -11,13 +14,16 @@ import personal.practice.skill_tech.repository.UserRepository;
 import personal.practice.skill_tech.web.dto.UserRegirstDto;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public User save(UserRegirstDto regirstDto) {
         User user = new User();
@@ -33,8 +39,13 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with email" + username) );
+                .orElseThrow(() -> new UsernameNotFoundException("Invalid username or password" + username) );
 
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), Collections.emptyList());
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), null);
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
 }
